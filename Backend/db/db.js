cfc6e38045f0f356e94a563d706cc9edd806da3c
Anjali kumari <anjalisinghbhanushali@gmail.com>
2026-01-30 12:35:18 +0530

    created user model commit

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-function connectToDb() {
-    mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => {
-            console.log('Connected to the database successfully');  
-        })
-        .catch(err => console.log(err));
-}
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CONNECT);
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log("DB connection error:", error);
+  }
+};
 
 module.exports = connectToDb;
